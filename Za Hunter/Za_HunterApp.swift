00f89bb927f33a441101c0b594271da00137a27b
Katherine Kelli <katherinekelli.creates@gmail.com>
2026-02-22 18:24:08 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Katherine Pizzirusso on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
