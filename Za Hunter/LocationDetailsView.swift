40c950d931323dce18fbaabba0961f2aa4dc5452
Katherine Kelli <katherinekelli.creates@gmail.com>
2026-02-25 09:40:37 -0600

    display location information

---
+++ Za Hunter/LocationDetailsView.swift
@@ -0,0 +1,31 @@
+//
+//  LocationDetailsView.swift
+//  Za Hunter
+//
+//  Created by Katherine Pizzirusso on 2/23/26.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailsView: View {
+    let mapItem: MKMapItem
+    var body: some View {
+        
+        let address = mapItem.placemark.subThoroughfare! + " " +
+        mapItem.placemark.thoroughfare! + "\n" +
+        mapItem.placemark.locality! + ", " +
+        mapItem.placemark.administrativeArea! + " " +
+        mapItem.placemark.postalCode!
+        VStack {
+            Text(mapItem.placemark.name!).font(.title).bold()
+            Text(address).padding()
+            Text(mapItem.phoneNumber!).padding()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LocationDetailsView(mapItem: MKMapItem())
+}
